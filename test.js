9701e7875793dba5969c20e7f7edddf904170db2
alex7alex <alexgorb7@gmail.com>
2024-01-24 11:06:25 +0200

    lesson 1 + 2 HW

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-let degreeFarenheit = ((+prompt("Please input degree")) * (9/5) + 32)
+// Lesson 1, Task 1
+let user_name = +prompt("Please input your name");
+console.log("Hello, "+user_name+ "! How are you?");
+
+
+//Lesson 2, Task 1
+ alert(((+prompt("Please input degree")) * (9/5) + 32));
