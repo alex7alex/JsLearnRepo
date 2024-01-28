c6c2d815461d78c703aaa9278f06e3adc391414a
alex7alex <alexgorb7@gmail.com>
2024-01-28 23:22:23 +0200

    lesson 3

--- test.js
+++ test.js
@@ -1,7 +1,26 @@
-// Lesson 1, Task 1
-let user_name = +prompt("Please input your name");
-console.log("Hello, "+user_name+ "! How are you?");
+// // Lesson 1, Task 1
+// let user_name = +prompt("Please input your name");
+// console.log("Hello, "+user_name+ "! How are you?");
 
 
-//Lesson 2, Task 1
- alert(((+prompt("Please input degree")) * (9/5) + 32));
+// //Lesson 2, Task 1
+//  alert(((+prompt("Please input degree")) * (9/5) + 32));
+
+ //Lesson 3, Task 1
+ let fingerNumber = +prompt("Please input a finger number");
+ if (fingerNumber === 1){
+    alert("The 1 finger is Thumb");
+ }
+ else if (fingerNumber === 2){
+    alert("The 2 finger is Index finger");
+ }
+ else if (fingerNumber === 3){
+    alert("The 3 finger is Middle finger");
+ }
+ else if (fingerNumber === 4){
+    alert("The 4 finger is Ring finger");
+ }
+ else if (fingerNumber === 5){
+    alert("The 5 finger is Little finger");
+ }
+ else alert ("Incorrect number")
